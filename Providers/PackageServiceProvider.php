5c2971e98c2f2b00ab11eed8d17bb8646a3c48c9
stephane_joussin <stephane@cartegriseminute.fr>
2023-03-31 01:51:37 +0200

    publishes

--- Providers/PackageServiceProvider.php
+++ Providers/PackageServiceProvider.php
@@ -2,14 +2,10 @@
 
 namespace Api\Providers;
 
-use Api\Generated\Database\Seeders\AddressSeeder;
+
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Event;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
 
 class PackageServiceProvider extends ServiceProvider
 {
@@ -24,7 +20,6 @@ class PackageServiceProvider extends ServiceProvider
         $this->commands(
             [
                 \Api\Console\Commands\Migrations\MigrationsMakeCommand::class,
-
                 \Api\Console\Commands\GenerateFromConfigMakeCommand::class,
 
                 \Api\Console\Commands\Types\ValidationRulesMakeCommand::class,
@@ -55,29 +50,7 @@ class PackageServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        JsonResource::withoutWrapping(); // pour les resources
-
-        //config
-        $this->mergeConfigFrom(base_path("src/config/laravel-scaffolder.php"), "laravel-scaffolder");
-
-        // routes
-        $this->loadRoutesFrom(base_path('src/Generated/routes/routes-swagger.php' ));
-
-        $generated_route_dir = base_path("src/Generated/routes/");
-        if (File::isDirectory($generated_route_dir)) {
-            $routesFiles = File::files(($generated_route_dir));
-            foreach ($routesFiles as $routesFile) {
-
-                if(env('ROUTES_RESOURCE') && Str::contains($routesFile->getRelativePathname(), "resource"))
-                {
-                    $this->loadRoutesFrom(base_path('src/Generated/routes/' . $routesFile->getRelativePathname()));
-
-                }
-
-            }
-        }
-
-
+        JsonResource::withoutWrapping();
     }
 
 }
--- Providers/PublishesServiceProvider.php
+++ Providers/PublishesServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Api\Providers;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class PublishesServiceProvider extends ServiceProvider
 {
@@ -15,21 +17,41 @@ class PublishesServiceProvider extends ServiceProvider
     {
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+
+    public function loadFromPackage()
+    {
+        //config
+        $this->mergeConfigFrom(base_path("src/config/laravel-scaffolder.php"), "laravel-scaffolder");
+
+        // views
+        $this->loadViewsFrom(base_path('src/Generated/resources/views/backoffice'), "laravel-scaffolder");
+
+        // swagger
+        $this->loadRoutesFrom(base_path('src/Generated/routes/routes-swagger.php' ));
+
+        // routes
+        $generated_route_dir = base_path("src/Generated/routes/");
+        if (File::isDirectory($generated_route_dir)) {
+            $routesFiles = File::files(($generated_route_dir));
+            foreach ($routesFiles as $routesFile) {
+                if(env('ROUTES_RESOURCE') && Str::contains($routesFile->getRelativePathname(), "resource"))
+                {
+                    $this->loadRoutesFrom(base_path('src/Generated/routes/' . $routesFile->getRelativePathname()));
+                }
+            }
+        }
+    }
+
+    public function publishPackage()
     {
         $dir = __DIR__ . '/../';
 
-        // publish the config base file
+        // publish the config
         $this->publishes([
             $dir . 'config/laravel-scaffolder.php' => config_path('laravel-scaffolder.php'),
         ], 'config');
 
-        // publish the default-template
+        // publish the views
         $this->publishes([
             $dir . 'Generated/resources/views/backoffice/' => base_path('resources/views/laravel-scaffolder'),
         ], 'views');
@@ -41,12 +63,23 @@ class PublishesServiceProvider extends ServiceProvider
         ], 'swagger');
 
 
-
         // publish the routes
         $this->publishes([
             $dir . 'Generated/routes/' => base_path("routes/laravel-scaffolder"),
         ], 'routes');
 
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        $this->loadFromPackage();
+        $this->publishPackage();
 
     }
 
